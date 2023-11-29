fde150510c2180eef12da34798cc3456b233fd7e
janSochaOfficial <janek111111666666@gmail.com>
2023-11-29 21:42:37 +0100

    small improvements

--- src/static/fuctions/sorting/insertion.ts
+++ src/static/fuctions/sorting/insertion.ts
@@ -4,25 +4,28 @@ export function* insertionSort(
   arr: number[]
 ): Generator<sortReturn, sortReturn> {
   for (let i = 1; i < arr.length; i++) {
-    for (let j = 0; j < i; j++) {
-      
-      if (arr[i] < arr[j]) {
-        const temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    let breakPoint = -1;
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[i] > arr[j]) {
+        breakPoint = j;
+        break;
+      } else {
         yield {
-            indexRead: [],
-            indexWrite: [j, i],
-            arr,
-          } as sortReturn;
+          indexRead: [j, i],
+          indexWrite: [],
+          arr,
+        } as sortReturn;
       }
-      else{
-        yield {
-            indexRead: [j, i],
-            indexWrite: [],
-            arr,
-          } as sortReturn;
-      }
+    }
+    for (let j = breakPoint + 1; j <= i; j++) {
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      yield {
+        indexRead: [],
+        indexWrite: [j, i],
+        arr,
+      } as sortReturn;
     }
   }
   return {
